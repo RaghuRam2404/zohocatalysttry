45f2fd3698bf72ee5f25335a354c98b6a3cd2cf6
Raghu Ram <raghuram2404@gmail.com>
2019-09-11 18:07:22 +0530

    Catalyst First Commit

---
+++ functions/FirstFunction/FirstFunction.java
@@ -0,0 +1,8 @@
+import com.zoho.cloud.function.Context;
+import com.zoho.cloud.function.basic.*;
+
+public class FirstFunction implements ZCFunction {
+    public void runner(Context context, BasicIO basicIO) throws Exception {
+        basicIO.write("Hello From FirstFunction.java");
+	}
+}
