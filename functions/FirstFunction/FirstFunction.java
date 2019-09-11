7bf2789a9f3ab2d2e44e2e30c005fdbfe9fe26f5
Raghu Ram <raghuram2404@gmail.com>
2019-09-11 19:55:44 +0530

    random number

--- functions/FirstFunction/FirstFunction.java
+++ functions/FirstFunction/FirstFunction.java
@@ -1,8 +1,9 @@
 import com.zoho.cloud.function.Context;
 import com.zoho.cloud.function.basic.*;
+import java.util.Random;
 
 public class FirstFunction implements ZCFunction {
     public void runner(Context context, BasicIO basicIO) throws Exception {
-        basicIO.write("Hello From FirstFunction.java from eclipse");
+        basicIO.write("Hello From FirstFunction.java from eclipse "+new Random().nextInt(1000));
 	}
 }
